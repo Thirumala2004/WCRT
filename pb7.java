77093f882157dc977f3232719e4602d9c6a65cf7
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2024-12-11 10:26:06 +0530

    Add files via upload

---
+++ pb7.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+public class pb7 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter an integer : ");
+        int num=sc.nextInt();
+        System.out.print("Enter the action string : ");
+        String act=sc.next();
+        String numstr=String.valueOf(num);
+        int numarr[]=new int[(numstr.length())];
+        for(int i=0;i<numstr.length();i++){
+            numarr[i]=(numstr.charAt(i)-48);
+        }
+        for(int i=0;i<numarr.length;i++){
+            System.out.print(numarr[i]+" ");
+        }
+        int j=0;
+        for(int i=0;i<act.length();i++){
+            char ch=act.charAt(i);
+            if (ch=='R') {
+                if(j<9){
+                    j=j+1;
+                }
+            }
+            else if (ch=='L') {
+                if(j!=0){
+                    j=j-1;
+                }
+            }
+            else if (ch=='T') {
+                numarr[j]=numarr[j]+1;
+            }
+            else if(ch=='B'){
+                numarr[j]=numarr[j]-1;
+            }
+            else if((ch=='0')||(ch=='1')||(ch=='2')||(ch=='3')||(ch=='4')||(ch=='5')||(ch=='6')||(ch=='7')||(ch=='8')||(ch=='9')){
+                continue;
+            }
+            else{
+                int temp=numarr[j];
+                numarr[j]=numarr[(act.charAt(i+1))-48];
+                numarr[(act.charAt(i+1))-48]=temp;
+            }
+        }
+        System.out.println();
+        for(int i=0;i<numarr.length;i++){
+            System.out.print(numarr[i]);
+        }
+    }
+}
