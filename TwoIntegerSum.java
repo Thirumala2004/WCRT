99c4beeec9bccabb1b56108b8b0536564a831725
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2025-06-23 12:18:19 +0530

    Update and rename sumnum.java to TwoIntegerSum.java

--- TwoIntegerSum.java
+++ TwoIntegerSum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class sumnum {
+public class TwoIntegerSum {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the Siz of array : ");
